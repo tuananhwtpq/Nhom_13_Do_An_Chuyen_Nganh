65a24a63ed589d83a966b96689f05f8995e52fb2
Duyycoder <duysusi872gmail.com>
2025-07-24 00:46:53 +0700

    make slidershow img in HomeFragment

--- app/src/main/java/com/example/foodordering/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/foodordering/Fragment/HomeFragment.kt
@@ -5,12 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
 
 import com.example.foodordering.R
+import com.example.foodordering.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
-
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +25,23 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val imgList = ArrayList<SlideModel>()
+        imgList.add(SlideModel(R.drawable.banner1,  ScaleTypes.FIT))
+        imgList.add(SlideModel(R.drawable.banner2, ScaleTypes.FIT))
+        imgList.add(SlideModel(R.drawable.banner3, ScaleTypes.FIT))
+
+        val imageSlider = binding.imageSlider
+        imageSlider.setImageList(imgList)
+        imageSlider.setImageList(imgList, ScaleTypes.FIT)
     }
     companion object{
 
--- app/src/main/java/com/example/foodordering/ui/ChoosenLocationActivity.kt
+++ app/src/main/java/com/example/foodordering/ui/ChoosenLocationActivity.kt
@@ -1,11 +1,13 @@
 package com.example.foodordering.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.foodordering.MainActivity
 import com.example.foodordering.R
 import com.example.foodordering.databinding.ActivityChoosenLocationBinding
 
@@ -22,5 +24,9 @@ class ChoosenLocationActivity : AppCompatActivity() {
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, locationList)
         val autoCompleteTextView = binding.listOfLocation
         autoCompleteTextView.setAdapter(adapter)
+        binding.textViewChooseLocationNext.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
